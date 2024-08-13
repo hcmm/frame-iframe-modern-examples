0382fb50835232c05b8d287e0ea728133e669a01
DESKTOP-CAH5QM0\henri <henriquecanto3@gmail.com>
2024-08-13 01:19:14 -0300

    removendo msgs

--- java-lib-auto-ecor-commons-events-rabbit/src/main/java/com/porto/ecor/mensageria/dto/DetalhesPedido.java
+++
@@ -1,8 +0,0 @@
-package com.porto.ecor.mensageria.dto;
-
-import lombok.Data;
-
-@Data
-public class DetalhesPedido<T> {
-    private T pedido;
-}
--- java-lib-auto-ecor-commons-events-rabbit/src/test/java/com/porto/ecor/mensageria/service/Test.java
+++
@@ -1,5 +0,0 @@
-package com.porto.ecor.mensageria.service;
-
-public class Test {
-
-}
